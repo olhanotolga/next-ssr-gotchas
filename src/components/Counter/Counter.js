310a33e2394568be83923ebb91f05cf1477aab16
olhanotolga <olha.halat@gmail.com>
2025-11-07 15:35:53 +0100

    fix: wait to client-render before accessing window object

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,23 @@
 'use client';
 import React from 'react';
+import Spinner from '../Spinner';
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
+
+  React.useEffect(() => {
+    const value = window.localStorage.getItem('saved-count');
+
+    setCount(value ? Number(value) : 0);
+  }, []);
 
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className='count-btn' onClick={() => setCount(count + 1)}>
+      Count: {count === null ? <Spinner /> : count}
     </button>
   );
 }
